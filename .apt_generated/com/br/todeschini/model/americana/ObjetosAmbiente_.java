e1bb05274d4aa1662fb8bcf4a9035f1bc00033a5
fernandobelem <fernando.abelem@gmail.com>
2016-10-13 14:58:44 -0300

    pomxml

---
+++ .apt_generated/com/br/todeschini/model/americana/ObjetosAmbiente_.java
@@ -0,0 +1,16 @@
+package com.br.todeschini.model.americana;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(ObjetosAmbiente.class)
+public abstract class ObjetosAmbiente_ {
+
+	public static volatile SingularAttribute<ObjetosAmbiente, Categoria> categoria;
+	public static volatile SingularAttribute<ObjetosAmbiente, String> nome;
+	public static volatile SingularAttribute<ObjetosAmbiente, Long> id;
+
+}
+
